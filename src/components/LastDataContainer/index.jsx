fa776900e91b9e1ca71c350d21310f2f046f48c0
camila-yniguez <cami.yniguez@gmail.com>
2023-05-30 00:00:23 -0300

    Fix dashboard

--- src/components/LastDataContainer/index.jsx
+++ src/components/LastDataContainer/index.jsx
@@ -16,7 +16,7 @@ export const LastDataContainer = ({type, data}) => {
 			setInfoToDisplay({
 				title: "Último usuario agregado: ",
 				name: data.name,
-				imageUrl: data.images[0].image,
+				// imageUrl: data.image,
 				description: data.description
 			})
 		}
@@ -42,7 +42,7 @@ export const LastDataContainer = ({type, data}) => {
 						<h6>{name}</h6>
 					</div>
 					<div className="text-center">
-						<img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: "40rem"}} src={`http//localhost:3001/images/productos/${imageUrl}`} alt="Imagen" />
+						<img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: "40rem"}} src={`http//localhost:3001/products`} alt="Imagen" />
 					</div>
 					<p>{description}</p>
 				</div>
